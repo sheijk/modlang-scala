21f2d69dd4fd83b233ea7dbf5808a6a301c4f496
Jan Rehders <nospam@sheijk.net>
2024-01-27 02:27:23 +0000

    added lexer

--- src/main/scala/me/modlang/Main.scala
+++ src/main/scala/me/modlang/Main.scala
@@ -19,33 +19,45 @@ def adt_demo() =
   run(comp)
   run(Expr.Plus(Expr.Constant(Value.I(10)), Expr.Constant(Value.I(20))))
 
-def andreMode() =
-  import scala.io.StdIn.readLine
+enum Token:
+  case Number(value: Int)
+  case Op(name : String)
 
+def toToken(str : String) : Token =
+  str.toIntOption match
+    case Some(i) => Token.Number(i)
+    case None => Token.Op(str)
+
+def andreMode() =
+  // import scala.io.StdIn.readLine
   // val source = readLine()
-  val tokens = "1  2 5 + +".split(" +") match
+  val source = "1  2 5 + +"
+  stackRun(source)
+
+def stackRun(source : String) =
+  val stringTokens = source.split(" +") match
     case l: Array[String|Null] =>
       l.map(x => x match
         case x : String => x
         case _ => throw Error("null token"))
     case _ => throw Error("lexer error")
+  val tokens = stringTokens.map(toToken)
 
   var stack : List[Value] = List()
 
   for (token <- tokens)
-    println(token)
-    token.toIntOption match
-      case Some(i) =>
+    println(s"read $token")
+    token match
+      case Token.Number(i) =>
         stack = Value.I(i) :: stack
-      case None =>
-        token match
-          case "+" =>
-            stack match
-              case lhs :: rhs :: rem =>
-                stack = adt_interpreter.interprete(Expr.Plus(Expr.Constant(lhs), Expr.Constant(rhs))) :: rem
-              case _ => throw Error("stack underflow" + stack)
-          case unknown =>
-            throw Error("invalid token " + unknown)
+      case Token.Op("+") =>
+        stack match
+          case lhs :: rhs :: rem =>
+            stack = interprete(Expr.Plus(Expr.Constant(lhs), Expr.Constant(rhs))) :: rem
+          case _ =>
+            throw Error("stack underflow" + stack)
+      case Token.Op(unknown) =>
+        throw Error("invalid token " + unknown)
 
   println("Stack:")
   stack.foreach(println)
