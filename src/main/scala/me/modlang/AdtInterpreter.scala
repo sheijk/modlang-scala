275c5a789f2fcfa1ee4d0653d59237cd18daea18
Jan Rehders <nospam@sheijk.net>
2024-01-27 01:07:56 +0000

    First mini interpreter using ADTs

---
+++ src/main/scala/me/modlang/AdtInterpreter.scala
@@ -0,0 +1,47 @@
+package me
+package modlang
+package adt_interpreter
+
+// import scala.io.StdIn.readLine
+
+enum Value:
+  case I(value : Int)
+  case B(value : Boolean)
+
+enum Expr:
+  case Constant(value : Value)
+  case And(lhs : Expr, rhs : Expr)
+  case Plus(lhs : Expr, rhs : Expr)
+  case GreaterThan(lhs : Expr, rhs : Expr)
+
+def interprete(e : Expr) : Value =
+  e match
+    case Expr.Constant(value) => value
+    case Expr.And(lhs, rhs) =>
+      val lhsValue = interprete(lhs)
+      val rhsValue = interprete(rhs)
+      (lhsValue, rhsValue) match
+        case (Value.B(lb), Value.B(rb)) =>
+          Value.B(lb && rb)
+        case _ => throw Error("Expected types Expr.And(bool, bool)")
+
+    case Expr.Plus(lhs, rhs) =>
+      val lhsValue = interprete(lhs)
+      val rhsValue = interprete(rhs)
+      (lhsValue, rhsValue) match
+        case (Value.I(li), Value.I(lr)) =>
+          Value.I(li + lr)
+        case _ => throw Error("Expected types Expr.Plus(int, int)")
+
+    case Expr.GreaterThan(lhs, rhs) =>
+      val lhsValue = interprete(lhs)
+      val rhsValue = interprete(rhs)
+      (lhsValue, rhsValue) match
+        case (Value.I(li), Value.I(lr)) =>
+          Value.B(li > lr)
+        case _ => throw Error("Expected types Expr.Plus(int, int)")
+
+def run(e : Expr) =
+  val value = interprete(e)
+  println(s"Got $value by running $e")
+
--- src/main/scala/me/modlang/Main.scala
+++ src/main/scala/me/modlang/Main.scala
@@ -1,9 +1,27 @@
 package me
 package modlang
 
+def adt_demo() =
+  import adt_interpreter.*
+
+  def int(v : Int) = Expr.Constant(Value.I(v))
+  def bool(v : Boolean) = Expr.Constant(Value.B(v))
+
+  import Expr.*
+
+  run(Plus(int(10), int(5)))
+  run(And(GreaterThan(int(10), int(5)), GreaterThan(int(3), int(2))))
+  run(And(bool(true), bool(false)))
+
+  val comp = Expr.And(Expr.GreaterThan(Expr.Constant(Value.I(10)), Expr.Constant(Value.I(5))),
+    Expr.GreaterThan(Expr.Constant(Value.I(10)), Expr.Constant(Value.I(5))))
+  run(comp)
+  run(Expr.Plus(Expr.Constant(Value.I(10)), Expr.Constant(Value.I(20))))
+
 @main def Main(args: String*): Unit =
   println("─" * 100)
+  println("hello nix")
 
-  println("hello world")
+  adt_demo()
 
   println("─" * 100)
