080624447d712be7c6d9a44819737eb318e66c83
Jan Rehders <nospam@sheijk.net>
2024-01-31 22:44:11 +0000

    Language providing mutable references

---
+++ src/main/scala/me/modlang/tfi/references.scala
@@ -0,0 +1,37 @@
+package me
+package modlang
+package tfi
+
+package References:
+  trait Ref[Expr]:
+    def set(v: Expr): Expr
+    def get(): Expr
+
+  trait Lang[T] extends Empty.Lang[T]:
+    def mut(name: String, value: Expr, in: Ref[Expr] => Expr): Expr
+
+  class StringRef(name: String) extends Ref[String]:
+    def set(v: String): String = s"($name = $v)"
+    def get(): String = name
+
+  trait ToStringMixin extends Lang[String], EvalId[String]:
+    def mut(name: String, value: Expr, in: Ref[Expr] => Expr): Expr =
+      s"(mut $name = $value :in ${in(new StringRef(name))})"
+
+  class ToString extends ToStringMixin, EvalId[String]
+  given ToString()
+
+  class FnRef[Value](initial: Value) extends Ref[() => Value]:
+    type Expr = () => Value
+    var value = initial
+    def set(v: Expr): Expr =
+      () =>
+        this.value = v()
+        value
+    def get(): Expr = () => value
+
+  trait EvalMixin[T] extends Lang[T], EvalFn[T]:
+    def mut(name: String, value: Expr, in: Ref[Expr] => Expr): Expr =
+      val ref = FnRef[Result](value())
+      () => in(ref)()
+
