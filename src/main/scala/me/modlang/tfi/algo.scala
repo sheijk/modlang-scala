2473b0423b077783a979ddd60d03313a1803f6d3
Jan Rehders <nospam@sheijk.net>
2024-01-31 21:26:39 +0000

    Move combined languages into their own files

--- src/main/scala/me/modlang/tfi/algo.scala
+++ src/main/scala/me/modlang/tfi/algo.scala
@@ -19,34 +19,3 @@ package Algo:
     def if_(cond: () => T, onTrue: Expr, onFalse: Expr): Expr =
       () => if asBool(cond) then onTrue() else onFalse()
 
-package Algo_calc:
-  trait Lang[T] extends Algo.Lang[T], Calc.Lang[T]
-
-  trait ToStringMixin extends Lang[String], Algo.ToStringMixin, Calc.ToStringMixin
-  class ToString extends ToStringMixin
-  given ToString()
-
-  trait EvalMixin[T] extends Lang[T] , Calc.EvalMixin[T] , Algo.EvalMixin[T]
-
-  type Value = Calc.Value
-  class Eval extends EvalMixin[Value], EvalFn[Value]
-  given Eval()
-
-  def tests() =
-    List(
-      (true,
-      [T] =>
-        (l: Lang[T]) =>
-          l.and(l.greaterThan(l.int(10), l.int(5)), l.greaterThan(l.int(3), l.int(2)))),
-      (10,
-      [T] =>
-        (l: Lang[T]) =>
-          l.if_(
-            l.greaterThan(l.int(10), l.int(5)),
-            l.plus(l.int(6), l.int(4)),
-            l.int(-1)
-          )
-        )
-    )
-
-  def testing() = tests().foreach(runTest[Calc.Value, Lang])
--- src/main/scala/me/modlang/tfi/bindings.scala
+++ src/main/scala/me/modlang/tfi/bindings.scala
@@ -17,35 +17,3 @@ package Bindings:
     type Expr = () => T
     def let(name: String, value: Expr, in: Expr => Expr): Expr =
       () => in(value)()
-
-package Algo_calc_bindings:
-  trait Lang[T] extends Bindings.Lang[T], Algo_calc.Lang[T]
-
-  trait ToStringMixin extends Lang[String], Algo_calc.ToStringMixin, Bindings.ToStringMixin
-  class ToString extends ToStringMixin
-  given ToString()
-
-  trait EvalMixin[T] extends Lang[T], Bindings.EvalMixin[T] , Algo_calc.EvalMixin[T]
-
-  type Value = Algo_calc.Value
-  class Eval extends EvalMixin[Value], EvalFn[Value]
-  given Eval()
-
-  def tests() =
-    List(
-      (108,
-      [T] => (l: Lang[T]) =>
-        import l.*
-        let("x", plus(int(5), int(3)), x =>
-          plus(int(100), x))),
-      (20,
-      [T] => (l: Lang[T]) => l.plus(l.int(5), l.int(15))),
-      (321,
-      [T] => (l: Lang[T]) =>
-        import l.*
-        let("x", plus(int(20), int(1)), x =>
-        let("y", plus(int(100), int(200)), y =>
-        plus(x, y)))),
-    )
-
-  def testing() = tests().foreach(runTest[Value, Lang])
