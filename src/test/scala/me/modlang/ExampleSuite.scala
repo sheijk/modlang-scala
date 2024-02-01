b871226a98af944db840e6a0cf918fb924b5c461
Jan Rehders <nospam@sheijk.net>
2024-02-01 01:14:34 +0000

    Remove unneeded example for Scala's test framework

--- src/test/scala/me/modlang/ExampleSuite.scala
+++
@@ -1,8 +0,0 @@
-package me
-package modlang
-
-final class ExampleSuite extends TestSuite:
-  test("hello world"):
-    forAll: (int: Int, string: String) =>
-      expectEquals(int, int)
-      expectEquals(string, string)
