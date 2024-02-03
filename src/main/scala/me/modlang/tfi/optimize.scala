140e24cc5f135642132f8fdbc6dd3f2d4e6a0120
Jan Rehders <nospam@sheijk.net>
2024-02-03 16:32:03 +0000

    combine constant folders for int and bool
    still broken

--- src/main/scala/me/modlang/tfi/optimize.scala
+++ src/main/scala/me/modlang/tfi/optimize.scala
@@ -36,62 +36,55 @@ package Optimizer:
   type Lang[T] = Calc.Lang[T]
   type Value = Calc.Value
 
-  transparent trait ConstantFoldIntMixin[T, Inner <: Lang[T]] extends Calc.Nested[T, Inner]:
-    type Expr = Either[inner.Expr, Int]
+  transparent trait ConstantFoldIntMixin[T, Inner <: Lang[T]] extends Calc.Nested[T, Inner]
+  transparent trait ConstantFoldBoolMixin[T, Inner <: Lang[T]] extends Calc.Nested[T, Inner]
+
+  transparent trait ConstantFoldMixin[T, Inner <: Lang[T]] extends ConstantFoldIntMixin[T, Inner], ConstantFoldBoolMixin[T, Inner]:
+    enum Expr:
+      case Dynamic(e: inner.Expr)
+      case ConstantInt(i: Int)
+      case ConstantBool(b: Boolean)
 
     def toOuter(e: inner.Expr): Expr =
-      Left(e)
-
-    def toInner(e: Expr): inner.Expr =
-      e match
-         case Left(innerExpr) => innerExpr
-         case Right(i) => inner.int(i)
+      Expr.Dynamic(e)
 
     override def int(v: Int): Expr =
-      Right(v)
-
-    override def plus(lhs: Expr, rhs: Expr): Expr =
-      (lhs, rhs) match
-      case (Right(lhsStatic), Right(rhsStatic)) => toOuter(inner.int(lhsStatic + rhsStatic))
-      case _ => toOuter(inner.plus(toInner(lhs), toInner(rhs)))
-
-  transparent trait ConstantFoldBoolMixin[T, Inner <: Lang[T]] extends Calc.Nested[T, Inner]:
-    type Expr = Either[inner.Expr, Boolean]
-
-    def toOuter(e: inner.Expr): Expr =
-      Left(e)
+      Expr.ConstantInt(v)
 
     def toInner(e: Expr): inner.Expr =
       e match
-         case Left(innerExpr) => innerExpr
-         case Right(i) => inner.bool(i)
+         case Expr.Dynamic(innerExpr) => innerExpr
+         case Expr.ConstantInt(i) => inner.int(i)
+         case Expr.ConstantBool(b) => inner.bool(b)
+
+    def toConstantInt(e: Expr) =
+      e match
+        case Expr.ConstantInt(i) => Some(i)
+        case _ => None
+
+    // No more knowledge about type Expr details
+
+    override def plus(lhs: Expr, rhs: Expr): Expr =
+      (toConstantInt(lhs), toConstantInt(rhs)) match
+      case (Some(lhsStatic), Some(rhsStatic)) => toOuter(inner.int(lhsStatic + rhsStatic))
+      case _ => toOuter(inner.plus(toInner(lhs), toInner(rhs)))
 
     override def bool(v: Boolean): Expr =
-      Right(v)
+      Expr.ConstantBool(v)
 
     override def and(lhs: Expr, rhs: Expr): Expr =
       (lhs, rhs) match
-      case (Right(lhsStatic), Right(rhsStatic)) => toOuter(inner.bool(lhsStatic & rhsStatic))
+      case (Expr.ConstantBool(lhsStatic), Expr.ConstantBool(rhsStatic)) => toOuter(inner.bool(lhsStatic & rhsStatic))
       case _ => toOuter(inner.and(toInner(lhs), toInner(rhs)))
 
-    // Doesn't work because we only know about static bool values here
-    // Need to implement this with flat composition and abstract over value type, again
-    // Maybe offer matching methods toConstantInt(e): Option[Int], etc.
-    // Or split into impl for Calc_bool, Calc_int, and Calc using both
-    // OCaml version only has one combined optimizer for Calc for exactly this reason
-    // Maybe optimizer needs to get an interpreter (it already has one as the inner parameter. but probably needs a Lang[Int|Boolean])
-    // Make an abstract interpreter which has type Value = Dynamic(innner.Expr) | Int | Boolean? maybe Dynamic can just be [...]
     override def greaterThan(lhs: Expr, rhs: Expr): Expr =
       (lhs, rhs) match
-      case (Right(lhsStatic), Right(rhsStatic)) => toOuter(inner.bool(lhsStatic > rhsStatic))
+      case (Expr.ConstantInt(lhsStatic), Expr.ConstantInt(rhsStatic)) => bool(lhsStatic > rhsStatic)
       case _ => toOuter(inner.greaterThan(toInner(lhs), toInner(rhs)))
 
-  case class ConstantFoldInt[T, Inner <: Lang[T]](inner_ : Inner) extends
+  case class ConstantFold[T, Inner <: Lang[T]](inner_ : Inner) extends
     Empty.Nested[T, Inner](inner_),
-    ConstantFoldIntMixin[T, Inner]
-  case class ConstantFoldBool[T, Inner <: Lang[T]](inner_ : Inner) extends
-    Empty.Nested[T, Inner](inner_),
-    ConstantFoldBoolMixin[T, Inner]
+    ConstantFoldMixin[T, Inner]
 
   case class ToStringCombine(from: Lang[String], to: Lang[String]) extends Lang[String]:
     type Expr = (from.Expr, to.Expr)
@@ -125,14 +118,8 @@ package Optimizer:
               l.and(l.bool(true), l.bool(true))),
       )
 
-  def foldInt(langs: (Lang[Value], Lang[String])): (Lang[Value], Lang[String]) =
-    (ConstantFoldInt(langs._1), ToStringCombine(langs._2, ConstantFoldInt(langs._2)))
-
-  def foldBool(langs: (Lang[Value], Lang[String])): (Lang[Value], Lang[String]) =
-    (ConstantFoldBool(langs._1), ToStringCombine(langs._2, ConstantFoldBool(langs._2)))
-
   def opt(langs: (Lang[Value], Lang[String])): (Lang[Value], Lang[String]) =
-    (ConstantFoldInt(ConstantFoldBool(langs._1)), ToStringCombine(langs._2, ConstantFoldInt(ConstantFoldBool(langs._2))))
+    (ConstantFold(langs._1), ToStringCombine(langs._2, ConstantFold(langs._2)))
 
   def demo(): Unit =
     println("Optimizer")
