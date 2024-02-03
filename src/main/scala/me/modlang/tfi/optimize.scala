d3112ed4fdd28120ad395bbfced2007f131744ea
Jan Rehders <nospam@sheijk.net>
2024-02-03 01:48:00 +0000

    factored out helper interface

--- src/main/scala/me/modlang/tfi/optimize.scala
+++ src/main/scala/me/modlang/tfi/optimize.scala
@@ -6,7 +6,30 @@ package Optimizer:
   type Lang[T] = Calc.Lang[T]
   type Value = Calc.Value
 
-  case class ConstantFoldInt[T, Inner <: Lang[T]](inner: Inner) extends Lang[T]:
+  trait Nested[T, Inner <: Lang[T]](inner_ : Inner) extends Lang[T]:
+    val inner = inner_
+    def toOuter(e: inner.Expr): Expr
+    def toInner(e: Expr): inner.Expr
+
+    override def eval(e: Expr): Result =
+      inner.eval(toInner(e))
+
+    override def int(v: Int): Expr =
+      toOuter(inner.int(v))
+
+    override def plus(lhs: Expr, rhs: Expr): Expr =
+      toOuter(inner.plus(toInner(lhs), toInner(rhs)))
+
+    override def and(lhs: Expr, rhs: Expr): Expr =
+      toOuter(inner.and(toInner(lhs), toInner(rhs)))
+
+    override def bool(v: Boolean): Expr =
+      toOuter(inner.bool(v))
+
+    override def greaterThan(lhs: Expr, rhs: Expr): Expr =
+      toOuter(inner.greaterThan(toInner(lhs), toInner(rhs)))
+
+  case class ConstantFoldInt[T, Inner <: Lang[T]](inner_ : Inner) extends Nested[T, Inner](inner_):
     type Expr = Either[inner.Expr, Int]
 
     def toOuter(e: inner.Expr): Expr =
@@ -24,18 +47,6 @@ package Optimizer:
       (lhs, rhs) match
       case (Right(lhsStatic), Right(rhsStatic)) => toOuter(inner.int(lhsStatic + rhsStatic))
       case _ => toOuter(inner.plus(toInner(lhs), toInner(rhs)))
-
-    override def eval(e: Expr): Result =
-      inner.eval(toInner(e))
-
-    override def and(lhs: Expr, rhs: Expr): Expr =
-      toOuter(inner.and(toInner(lhs), toInner(rhs)))
-
-    override def bool(v: Boolean): Expr =
-      toOuter(inner.bool(v))
-
-    override def greaterThan(lhs: Expr, rhs: Expr): Expr =
-      toOuter(inner.greaterThan(toInner(lhs), toInner(rhs)))
 
   case class ToStringCombine(from: Lang[String], to: Lang[String]) extends Lang[String]:
     type Expr = (from.Expr, to.Expr)
