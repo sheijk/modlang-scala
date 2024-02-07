a039c9fb6a9b536b6efcf2f43dbc627f41d307ba
Jan Rehders <nospam@sheijk.net>
2024-02-07 01:04:19 +0000

    Get rid of helper functions needed by References.Dup/Nested

--- src/main/scala/me/modlang/tfi/references.scala
+++ src/main/scala/me/modlang/tfi/references.scala
@@ -11,18 +11,26 @@ package References:
     def mut(name: String, value: Expr, in: Ref[Expr] => Expr): Expr
 
   transparent trait Nested[T, Inner <: Lang[T]] extends Lang[T], Empty.Nested[T, Inner]:
-    def toOuterRef(r: Ref[inner.Expr]): Ref[Expr]
+    class NestedRef(innerRef: Ref[inner.Expr]) extends Ref[Expr]:
+      def get(): Expr = toOuter(innerRef.get())
+      def set(v: Expr) = toOuter(innerRef.set(toInner(v)))
 
     def mut(name: String, value: Expr, in: Ref[Expr] => Expr): Expr =
-      toOuter(inner.mut(name, toInner(value), v => toInner(in(toOuterRef(v)))))
+      toOuter(inner.mut(name, toInner(value), v => toInner(in(NestedRef(v)))))
 
   trait Dup[T, L <: Lang[T]] extends Lang[T], Empty.Dup[T, L]:
-    def fromLeftRef(r: Ref[left.Expr]): Ref[Expr]
-    def fromRightRef(r: Ref[right.Expr]): Ref[Expr]
+    class PairRef(leftRef: Ref[left.Expr], rightRef: Ref[right.Expr]) extends Ref[Expr]:
+      def set(v: Expr): Expr = (leftRef.set(v._1), rightRef.set(v._2))
+      def get(): Expr = (leftRef.get(), rightRef.get())
 
     def mut(name: String, value: Expr, in: Ref[Expr] => Expr): Expr =
-      (left.mut(name, value._1, v => in(fromLeftRef(v))._1),
-      right.mut(name, value._2, v => in(fromRightRef(v))._2))
+      var result : Expr | Null = null
+      left.mut(name, value._1, leftRef =>
+        right.mut(name, value._2, rightRef =>
+          result = in(PairRef(leftRef, rightRef))
+          result.asInstanceOf[Expr]._2)
+        result.asInstanceOf[Expr]._1)
+      result.asInstanceOf[Expr]
 
   class StringRef(name: String) extends Ref[String]:
     def set(v: String): String = s"($name = $v)"
