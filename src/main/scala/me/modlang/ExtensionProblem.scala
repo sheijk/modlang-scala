f349562e93f0a045b48c1173fa8197b97a3d31be
Jan Rehders <nospam@sheijk.net>
2024-02-13 00:30:38 +0000

    Add traits based solution
    Based on "Independently Extensible Solutions to the Expression Problem" by
    M. Zenger, M. Odersky

--- src/main/scala/me/modlang/ExtensionProblem.scala
+++ src/main/scala/me/modlang/ExtensionProblem.scala
@@ -107,6 +107,48 @@ package visitor_model:
     val e = Add(Lit(10), Neg(Lit(5)))
     println(s"  eval(${e.accept(ShowN())}) => ${e.accept(EvalN())} [visitor]")
 
+// Traits based solution based on "Independently Extensible Solutions to the
+// Expression Problem" by M. Zenger, M. Odersky
+package traits_model:
+  trait Base:
+    type Expr <: ExprBase
+    trait ExprBase:
+      def show(): String
+
+    class Lit(value: Int) extends ExprBase:
+      def show(): String = value.toString()
+
+    class Add(lhs: Expr, rhs: Expr) extends ExprBase:
+      def show(): String = s"${lhs.show()} + ${rhs.show()}"
+
+  trait Neg extends Base:
+    class Neg(e: Expr) extends ExprBase:
+      def show(): String = s"-${e.show()}"
+
+  trait Eval extends Base:
+    type Expr <: ExprEval
+    trait ExprEval extends ExprBase:
+      def eval(): Int
+
+    class LitE(value: Int) extends Lit(value), ExprEval:
+      def eval(): Int = value
+
+    class AddE(lhs: Expr, rhs: Expr) extends Add(lhs, rhs), ExprEval:
+      def eval(): Int = lhs.eval() + rhs.eval()
+
+  trait EvalNeg extends Eval, Neg:
+    class NegE(e: Expr) extends Neg(e), ExprEval:
+      def eval(): Int = -e.eval()
+
+  class L extends EvalNeg:
+    type Expr = ExprEval
+
+  def test() =
+    val l = L()
+    import l.*
+    val e = AddE(LitE(10), NegE(LitE(5)))
+    println(s"  eval(${e.show()}) => ${e.eval()} [traits]")
+
 // Read source [9] from "The extension problem revisited" by Mads Torgensen and
 // add functional approaches here. Solutions for immutable objects only are
 // allowed.
@@ -148,5 +190,6 @@ def demo() =
   println("Extension problem")
   class_model.test()
   visitor_model.test()
+  traits_model.test()
   tfi_model.test()
 
