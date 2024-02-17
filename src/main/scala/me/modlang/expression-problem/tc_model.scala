ee245044fcf72e75e7056cc1d711647fb719dd18
Jan Rehders <nospam@sheijk.net>
2024-02-17 01:10:46 +0000

    Add a simple opt with context to type class solution

--- src/main/scala/me/modlang/expression-problem/tc_model.scala
+++ src/main/scala/me/modlang/expression-problem/tc_model.scala
@@ -9,12 +9,12 @@ package tc_model:
 
   given showLang: Lang[String] = new Lang[String]:
     def lit(value: Int): String = value.toString()
-    def add(lhs: String, rhs: String): String = s"$lhs + $rhs"
+    def add(lhs: String, rhs: String): String = s"($lhs + $rhs)"
 
   trait LangN[T]:
     def neg(e: T): T
 
-  implicit val showLangN: LangN[String] = new LangN[String]:
+  given showLangN: LangN[String] = new LangN[String]:
     def neg(e: String): String = s"-$e"
 
   given evalLang: Lang[Int] = new Lang[Int]:
@@ -31,10 +31,26 @@ package tc_model:
     extension [T](lhs: T) def +(rhs: T)(using l: Lang[T]): T = l.add(lhs, rhs)
   import syntax.*
 
+  enum CtxNeg { case Pos; case Neg }
+
+  given pushDown[T](using l: Lang[T]) : Lang[CtxNeg => T] = new Lang[CtxNeg => T]:
+    def lit(value: Int) : CtxNeg => T =
+      case CtxNeg.Pos => l.lit(value)
+      case CtxNeg.Neg => l.lit(-value)
+    def add(lhs: CtxNeg => T, rhs: CtxNeg => T): CtxNeg => T =
+      case CtxNeg.Pos => l.add(lhs(CtxNeg.Pos), rhs(CtxNeg.Pos))
+      case CtxNeg.Neg => l.add(lhs(CtxNeg.Neg), rhs(CtxNeg.Neg))
+
+  given pushDownN[T](using l: LangN[T]) : LangN[CtxNeg => T] = new LangN[CtxNeg => T]:
+    def neg(e: CtxNeg => T): CtxNeg => T =
+      case CtxNeg.Pos => e(CtxNeg.Neg)
+      case CtxNeg.Neg => e(CtxNeg.Pos)
+
+  def pushNeg[T](e: CtxNeg => T): T = e(CtxNeg.Pos)
+
   def test() =
-    def e[T : Lang : LangN]: T = lit(10) + neg(lit(5))
-    // def e[T : Lang : LangN]: T = lit(10) + lit(neg(5))
-    // ^- not sure why neg(5) is legal here. Looks like evalLang typeclass is
-    // pulled in and used here
-    println(s"  eval(${e[String]}) => ${e[Int]} [typeclass]")
+    def ex[T : Lang : LangN] = neg(lit(-10) + neg(lit(5)))
+    val negStr: String = pushNeg(ex)
+    val result: Int = pushNeg(ex)
+    println(s"  eval(${ex[String]}) =opt=> eval(${negStr}) = ${result} [typeclass]")
 
