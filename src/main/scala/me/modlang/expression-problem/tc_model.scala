e3a01bde6edb7b1c235a62722277544e9e70c139
Jan Rehders <nospam@sheijk.net>
2024-02-17 02:51:48 +0000

    factor out some code

--- src/main/scala/me/modlang/expression-problem/tc_model.scala
+++ src/main/scala/me/modlang/expression-problem/tc_model.scala
@@ -60,24 +60,28 @@ package tc_model:
         case Dynamic(t) => Dynamic(fT(t))
         case ConstantInt(value) => ConstantInt(fInt(value))
 
+  object Folded:
+    def map2[T](lhs: Folded[T], rhs: Folded[T], f: (T, T) => T, fConstant: (Int, Int) => Int)(using l: Lang[T]): Folded[T] =
+      (lhs, rhs) match
+      case (Folded.ConstantInt(lhsValue), Folded.ConstantInt(rhsValue)) =>
+        Folded.ConstantInt(fConstant(lhsValue, rhsValue))
+      case (_, _) =>
+        Folded.Dynamic(f(lhs.to(l), rhs.to(l)))
+
   trait LangAndN[T] extends Lang[T], LangN[T]
   given cf[T](using l: Lang[T], n: LangN[T]): LangAndN[Folded[T]] = new LangAndN[Folded[T]]:
     def lit(value: Int) = Folded.ConstantInt(value)
     def add(lhs: Folded[T], rhs: Folded[T]): Folded[T] =
-      (lhs, rhs) match
-      case (Folded.ConstantInt(lhsValue), Folded.ConstantInt(rhsValue)) =>
-        Folded.ConstantInt(lhsValue + rhsValue)
-      case (_, _) =>
-        Folded.Dynamic(l.add(lhs.to(l), rhs.to(l)))
+      Folded.map2(lhs, rhs, l.add, (l, r) => l + r)
     def neg(e: Folded[T]): Folded[T] =
       e.map(n.neg, i => -i)
 
-  def fold(e: Folded[String]): String = e.to(showLang)
+  def fold[T](e: Folded[T])(using l: Lang[T]): T = e.to(l)
 
   def test() =
     def ex[T : Lang : LangN] = neg(lit(-10) + neg(lit(5)))
     val negStr: String = fold(ex)
     // val negStr: String = pushNeg(ex)
-    val result: Int = pushNeg(ex)
+    val result: Int = fold(pushNeg(ex))
     println(s"  eval(${ex[String]}) =opt=> eval(${negStr}) = ${result} [typeclass]")
 
