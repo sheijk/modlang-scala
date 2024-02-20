aad74a0edbb949e552904269310e25731e778aba
Jan Rehders <nospam@sheijk.net>
2024-02-20 22:08:05 +0000

    wip symbols

--- src/main/scala/me/modlang/tfi/symbols.scala
+++ src/main/scala/me/modlang/tfi/symbols.scala
@@ -3,11 +3,10 @@ package modlang
 package tfi
 
 package Empty:
-  type Context[Ctx, T] = (Ctx, T)
   trait PushDown[T, Payload] extends Lang[Payload => T]
 
-package Imperative:
-  type Payload = Int
+package Calc_bool:
+  case class Payload(value: Int)
   trait PushDown[T](val next: Lang[T]) extends Lang[Payload => T], Empty.PushDown[T, Payload]:
     val l = next
     type Expr = Payload => l.Expr
@@ -18,6 +17,17 @@ package Imperative:
         l.and(lhs(ctx), rhs(ctx))
         l.and(lhs(ctx), rhs(ctx))
 
+    // def eval(e: Expr): Result = l.eval(e(l)(Payload(0)))
+    def eval(e: Expr): Payload => Result =
+      val x: l.Expr = e(Payload(0))
+      val r: T = l.eval(x)
+      r
+
+    // l.eval(e(Payload(0)))
+  given pushDown[T](using l: Lang[T]) : PushDown[T](l) with {}
+
+// package Imperative:
+//   trait PushDown[T](val next: Lang[T]) extends Lang[Payload => T], Empty.PushDown[T, Payload]:
     // def int(v: Int): Expr
     // def plus(lhs: Expr, rhs: Expr): Expr
 
@@ -36,13 +46,26 @@ package Imperative:
     // def dummy(msg: String, e: Expr): Expr
 
 package Symbols:
-  trait Lang[T] extends Empty.Lang[T]
+  // trait Lang[T] extends Empty.Lang[T]
+  // 
+  // transparent trait Nested[T, Inner <: Lang[T]] extends Lang[T], Empty.Nested[T, Inner]
+  // 
+  // trait Dup[T, L <: Lang[T]] extends Lang[T], Empty.Dup[T, L]
+  // 
+  // trait ToStringMixin extends Lang[String], EvalId[String]
+  // given ToStringMixin with EvalId[String] with {}
+  // 
+  // trait EvalMixin[T] extends Lang[T], EvalFn[T]
 
-  transparent trait Nested[T, Inner <: Lang[T]] extends Lang[T], Empty.Nested[T, Inner]
+  def demo() =
+    println("Symbols & Context")
+    import Calc_bool.{*, given}
 
-  trait Dup[T, L <: Lang[T]] extends Lang[T], Empty.Dup[T, L]
-
-  trait ToStringMixin extends Lang[String], EvalId[String]
-  given ToStringMixin with EvalId[String] with {}
-
-  trait EvalMixin[T] extends Lang[T], EvalFn[T]
+    // import CaptureLocation.f
+    type Ast = [T] => (l: Lang[T]) => l.Expr
+    val ast = [T] => (l: Lang[T]) => l.bool(true)
+    // runProgram[Boolean, Lang](ast)
+    def run[T](ast: Ast)(using l : Lang[T]) = l.eval(ast(l))
+    val src: String = run(ast)
+    val r: Payload => Boolean = run(ast)
+    println(s"$src => $r")
