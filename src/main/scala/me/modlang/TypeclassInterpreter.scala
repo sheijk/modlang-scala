325810fbadf249f53f31c90a92274240622a2adc
Jan Rehders <nospam@sheijk.net>
2024-02-21 00:34:23 +0000

    added eval

--- src/main/scala/me/modlang/TypeclassInterpreter.scala
+++ src/main/scala/me/modlang/TypeclassInterpreter.scala
@@ -4,7 +4,6 @@ package typeclass
 
 package base:
   trait Show[T]:
-    type E = T
     def show(e: T): String
 
   def show[T: Show](t: T): String = summon[Show[T]].show(t)
@@ -25,7 +24,30 @@ package base:
     printSource(Lit(10))
     printSource(Add(Lit(1), Lit(2)))
 
+package eval:
+  import base.{*, given}
+
+  trait Eval[Value, E]:
+    def eval(e: E): Value
+
+  given evalLit[T] : Eval[T, Lit[T]] with
+    type Value = T
+    def eval(t: Lit[T]): T = t.value
+
+  given evalAdd[E](using e: Eval[Int, E]) : Eval[Int, Add[E]] with
+    def eval(t: Add[E]): Int = e.eval(t.lhs) + e.eval(t.rhs)
+
+  def run[T](e: T)(using eval : Eval[Int, T])(using show: Show[T]) =
+    val src = show.show(e)
+    val r = eval.eval(e)
+    println(s"  $src => $r")
+
+  def demo() =
+    run(Lit(10))
+    run(Add(Lit(1), Lit(2)))
+
 def demo() =
   println("Type classes")
   base.demo()
+  eval.demo()
 
