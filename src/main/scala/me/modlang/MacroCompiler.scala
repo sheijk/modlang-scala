28803faa1a7562ab7664afe1c91cb7d63c727c4e
Jan Rehders <nospam@sheijk.net>
2024-02-22 14:05:00 +0000

    New design for macro compiler

--- src/main/scala/me/modlang/MacroCompiler.scala
+++ src/main/scala/me/modlang/MacroCompiler.scala
@@ -2,62 +2,80 @@ package me
 package modlang
 package macro_compiler
 
-type Value = Int | Boolean
+trait Language[InEx, OutEx]:
+  type I = InEx
+  type O = OutEx
+  // trait Builtin:
+  //   type Ex <: OutEx
+  //   def create(args: List[InEx]): Ex
 
-opaque type BaseExpr = String
+  type Context
+  def initialContext(): Context
 
-sealed trait AstType[Output]
-trait Builtin[Output](val name: String) extends AstType[Output]:
-  def expand(e: Symex): Output
-  override def toString() = s"Builtin $name"
-trait Macro[Output](val name: String) extends AstType[Output]
+  def translate(ctx: Context, ex: InEx): OutEx
+  def translateAll(ctx: Context, exs: List[InEx]): List[OutEx] =
+    val ctx = initialContext()
+    exs.map(translate(ctx, _))
 
-enum Expr[Head]:
-  case Atom(hd: Head)
-  case List(childs: List[Expr[Head]])
-type Symex = Expr[String]
+type SingleValue = Int | Boolean | String
+type Value = SingleValue | List[SingleValue]
+type Program = () => Value
 
-case class Language[Output](name: String, builtins: Map[String, Builtin[Output]]):
-  def lookup(id: String): Option[Builtin[Output]] =
-    builtins get id
-  def expand(e: Symex): Output =
-    e match
-      case Expr.Atom(hd) =>
-        lookup(hd) match
-          case Some(builtin : Builtin[Output]) => builtin.expand(e)
+enum SymEx:
+  case S(v: String)
+  case Ls(l: List[SymEx])
 
-  def compile(p: List[Symex]): List[Output] =
-    p.map(expand(_))
+def compile(lang: Language[SymEx, Program], ast: lang.I): lang.O =
+  lang.translate(lang.initialContext(), ast)
 
-def language[Output](name: String, builtins: Builtin[Output]*): Language[Output] =
-  Language(name, builtins.map((b: Builtin[Output]) => (b.name, b)).toMap)
+def runAndPrint(lang: Language[SymEx, Program], ast: lang.I) =
+  val program = compile(lang, ast)
+  val result = program()
+  println(s"eval($ast) = $result")
 
-case class Program[Ex](
-  name: String,
-  language: Language[Ex],
-  exprs: List[Ex],
-):
-  override def toString(): String =
-    val body = exprs.map(_.toString).mkString("\n  ")
-    s"Program $name :language ${language.name} {\n  $body\n}"
+case class HelloLanguage() extends Language[SymEx, Program]:
+  val nothing: Value = List()
+  case class Context():
+    var silent = false
+    var name: Option[String] = None
+    def hello(): Value =
+      (silent, name) match
+        case (true, _) => nothing
+        case (false, None) => "hello!"
+        case (false, Some(name)) => s"hello $name!"
+  def initialContext() = Context()
 
-type OutputLang = () => Unit
-type UntypedProgram = List[Symex]
-type TypedProgram = List[OutputLang]
-
-def run(l: Language[OutputLang], ast: List[Symex]) =
-  val out: List[OutputLang] = l.compile(ast)
-  out.foreach((program: OutputLang) =>
-    println(s"Running $ast")
-    program())
-
-val langHello =
-  val hello = new Builtin[OutputLang]("hello"):
-    def expand(e: Symex): OutputLang = () => println("Hello!")
-  val shht = new Builtin[OutputLang]("shht"):
-    def expand(e: Symex): OutputLang = () => println("shht!")
-  language("langHello", hello, shht)
+  def translate(ctx: Context, ex: I): Program =
+    ex match
+      case SymEx.S("hello") =>
+        val msg = ctx.hello()
+        () => msg
+      case SymEx.S("shhht") =>
+        ctx.silent = true
+        () => nothing
+      case SymEx.Ls(List(SymEx.S("name"), SymEx.S(name))) =>
+        ctx.name = Some(name)
+        () => nothing
+      case SymEx.Ls(exs) =>
+        val nestedCtx = ctx.copy()
+        val outs: List[Value] = exs.map(translate(nestedCtx, _)())
+        def toValues(v: Value): List[SingleValue] =
+          v match
+          case l: List[SingleValue] => l
+          case s: SingleValue => List(s)
+        val flat: List[SingleValue] = outs.flatMap(toValues)
+        () => flat
+      case _ => ???
 
 def demo() =
   println("Macro compiler")
-  run(langHello, List(Expr.Atom("hello"), Expr.Atom("hello")))
+  val helloL = HelloLanguage()
+  def sym(x: String|SymEx): SymEx = x match { case s: String => SymEx.S(s) case x: SymEx => x}
+  def l(args: (String|SymEx)*) = SymEx.Ls(args.map(sym).toList)
+  def hello = sym("hello")
+  def name(n: String) = l("name", n)
+  def shhht = sym("shhht")
+  runAndPrint(helloL, sym("hello"))
+  runAndPrint(helloL, l("hello", "hello", "hello"))
+  runAndPrint(helloL, l("hello", "shhht", "hello"))
+  runAndPrint(helloL, l(name("foo"), hello, hello, name("bar"), hello, shhht, hello, hello))
