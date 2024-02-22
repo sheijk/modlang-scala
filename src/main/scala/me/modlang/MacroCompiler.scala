bf9b46b31893f18e1f6af1338d62f3b7d068f7bc
Jan Rehders <nospam@sheijk.net>
2024-02-22 15:38:36 +0000

    add macro language infrastructure

--- src/main/scala/me/modlang/MacroCompiler.scala
+++ src/main/scala/me/modlang/MacroCompiler.scala
@@ -5,9 +5,6 @@ package macro_compiler
 trait Language[InEx, OutEx]:
   type I = InEx
   type O = OutEx
-  // trait Builtin:
-  //   type Ex <: OutEx
-  //   def create(args: List[InEx]): Ex
 
   type Context
   def initialContext(): Context
@@ -24,6 +21,7 @@ type Program = () => Value
 enum SymEx:
   case S(v: String)
   case Ls(l: List[SymEx])
+  def id() = this match { case S(name) => Some(name) case _ => None }
 
 def compile(lang: Language[SymEx, Program], ast: lang.I): lang.O =
   lang.translate(lang.initialContext(), ast)
@@ -33,19 +31,48 @@ def runAndPrint(lang: Language[SymEx, Program], ast: lang.I) =
   val result = program()
   println(s"eval($ast) = $result")
 
-case class HelloLanguage() extends Language[SymEx, Program]:
-  val nothing: Value = List()
+trait Builtin[Context, InEx, OutEx]:
+  val name: String
+  def create(ctx: Context, expr: InEx): Option[OutEx]
+
+trait MacroLanguage[Context, OutEx](builtins: List[Builtin[Context, SymEx, OutEx]]) extends Language[SymEx, OutEx]:
+  val builtinMap: Map[String, Builtin[Context, SymEx, OutEx]] = builtins.map(b => (b.name, b)).toMap
+
+  def translateBuiltin(ctx: Context, ast: SymEx): Option[OutEx] =
+    ast.id().flatMap(builtinMap get _).flatMap(_.create(ctx, ast))
+
+  def compilerError(msg: String): OutEx
+  def translate(ctx: Context, ex: SymEx): O =
+    translateBuiltin(ctx, ex).getOrElse(compilerError(s"Unknown ast id ${ex.id().getOrElse("*List*")}"))
+
+object HelloLanguage:
   case class Context():
     var silent = false
     var name: Option[String] = None
     def hello(): Value =
       (silent, name) match
-        case (true, _) => nothing
+        case (true, _) => List()
         case (false, None) => "hello!"
         case (false, Some(name)) => s"hello $name!"
-  def initialContext() = Context()
+  type HelloBuiltin = Builtin[Context, SymEx, Program]
+  val helloB = new HelloBuiltin:
+    val name: String = "hello"
+    def create(ctx: Context, expr: SymEx): Option[Program] = ???
+  val shhhtB = new HelloBuiltin:
+    val name: String = "shhht"
+    def create(ctx: Context, expr: SymEx): Option[Program] = ???
+  val nameB = new HelloBuiltin:
+    val name: String = "name"
+    def create(ctx: Context, expr: SymEx): Option[Program] = ???
+  val builtins: List[Builtin[Context, SymEx, Program]] = List(helloB, shhhtB, nameB)
 
-  def translate(ctx: Context, ex: I): Program =
+case class HelloLanguage() extends MacroLanguage[HelloLanguage.Context, Program](HelloLanguage.builtins):
+  val nothing: Value = List()
+  type Context = HelloLanguage.Context
+  def initialContext() = HelloLanguage.Context()
+
+  def compilerError(msg: String): Program = () => s"error: $msg"
+  override def translate(ctx: Context, ex: I): Program =
     ex match
       case SymEx.S("hello") =>
         val msg = ctx.hello()
@@ -65,7 +92,8 @@ case class HelloLanguage() extends Language[SymEx, Program]:
           case s: SingleValue => List(s)
         val flat: List[SingleValue] = outs.flatMap(toValues)
         () => flat
-      case _ => ???
+      case _ =>
+        super.translate(ctx, ex)
 
 def demo() =
   println("Macro compiler")
