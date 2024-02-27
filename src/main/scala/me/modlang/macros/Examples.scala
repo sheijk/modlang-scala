12d1d24dd2339892909204ad3a5f9fb0b923f90e
Jan Rehders <nospam@sheijk.net>
2024-02-27 01:54:30 +0000

    Tests use SymEx.parse

--- src/main/scala/me/modlang/macros/Examples.scala
+++ src/main/scala/me/modlang/macros/Examples.scala
@@ -5,15 +5,19 @@ package macro_compiler
 def examples: List[(SymEx, Value, tfi.Location)] =
   import SymEx.*
   def greet(n: String) = l(name(n), hello)
+  def p(s: String) =
+    SymEx.parse(s) match
+    case Left(e) => throw Error(e.toString)
+    case Right(ex) => ex
   import tfi.CaptureLocation.f
   List(
-    f(sym("hello"),
+    f(p("hello"),
      "hello!"),
-    f(seq("hello", "hello", "hello"),
+    f(p("seq hello hello hello"),
      List("hello!", "hello!", "hello!")),
-    f(seq("hello", "shhht", "hello"),
+    f(p("seq hello shhht hello"),
      List("hello!")),
-    f(seq(name("foo"), hello, hello, name("bar"), hello, shhht, hello, hello),
+    f(p("seq (name foo) hello hello (name bar) hello shhht hello hello"),
      List("hello foo!", "hello foo!", "hello bar!")),
     f(seq(janMode, helloJan, hello),
      List("hello Jan!", "hello!")),
@@ -21,35 +25,35 @@ def examples: List[(SymEx, Value, tfi.Location)] =
      List("hello Jan!", "error: Unknown id helloJan in helloJan")),
 
     f(seq(
-      l("defmacro", l("hifoo"), l(name("foo"), hello)),
+      p("defmacro (hifoo) ((name foo) hello)"),
       l("hifoo")),
      List("hello foo!")),
     f(seq(
-      l("defmacro", l("hifoo"), l(name("foo"), hello)),
+      p("defmacro (hifoo) ((name foo) hello)"),
       sym("hifoo")),
      List("hello foo!")),
 
     f(seq(
-      l("defmacro", l("swap", "$left", "$right"), "$right", "$left"),
+      p("defmacro (swap $left $right) $right $left"),
       l("swap", greet("2nd"), greet("1st"))),
      List("hello 1st!", "hello 2nd!")),
     f(seq(
-      l("defmacro", l("swap", "$left", "$right"), "$right", "$left"),
+      p("defmacro (swap $left $right) $right $left"),
       l("swap")),
     List("error: Expected (swap $left $right) in ((swap))")),
     f(seq(
-      l("defmacro", l("swap", "$left", "$right"), "$right", "$left"),
+      p("defmacro (swap $left $right) $right $left"),
       l("swap", "too", "many", "args")),
      List("error: Expected (swap $left $right) in ((swap too many args))")),
 
     // Pattern macros
     f(seq(
-      l("defmacro", l("foo", "1", "2", "$three"), sym("$three")),
-      l("foo", "1", "2", l(name("3rd"), hello))),
+      p("(defmacro (foo 1 2 $three) $three)"),
+      p(s"(foo 1 2 ${l(name("3rd"), hello)})")),
       List("hello 3rd!")),
     f(seq(
-      l("defmacro", l("twice", l("name", "$name")), l(name("$name"), "hello", "hello")),
-      l("twice", name("foobar"))),
+      p("(defmacro (twice (name $name)) ((name $name) hello hello))"),
+      p("(twice (name foobar))")),
       List("hello foobar!", "hello foobar!")),
    )
 
@@ -57,10 +61,3 @@ def demo() =
   println("Macro compiler")
   def helloL = HelloLanguage()
   examples.foreach((ex, _, _) => helloL.runAndPrint(ex))
-  def p(s: String) =
-    val result = SymEx.parse(s)
-    println(s"parsed $result from '$s'")
-  p("foo ")
-  p("foo bar")
-  p("(a b c)")
-  p("(defun (foo x y) (ret 0))")
