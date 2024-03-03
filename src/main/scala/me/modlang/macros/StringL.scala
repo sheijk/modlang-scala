0b4f730cc3194c134cf6bf55b3107b8bc30625b3
Jan Rehders <nospam@sheijk.net>
2024-03-03 02:46:14 +0000

    Use rewrite rules for parser

--- src/main/scala/me/modlang/macros/StringL.scala
+++ src/main/scala/me/modlang/macros/StringL.scala
@@ -14,10 +14,33 @@ package StringL:
       val s = SimpleScope[Parser](None)
       addBuiltins(s)
       s
+    def rules(): List[Parser] = List()
     def addBuiltins(s: SymbolTable): Unit
     def parseString(str: String): Option[lang.Expr]
 
+    def rewriteRule(pattern: SymEx, replace: Map[String, lang.Expr] => lang.Expr): SymEx => Option[lang.Expr] =
+      ex =>
+        ex.bindIdsInPattern(pattern) match
+        case Right(replacements) =>
+          val replTs = replacements.map((name, repl) => (name, parse(repl).getOrElse(throw Error("foo")))).toMap
+          Some(replace(replTs))
+        case Left(_) => None
+
+    def rewriteRule(pattern: String, replace: Map[String, lang.Expr] => lang.Expr): SymEx => Option[lang.Expr] =
+      val patternEx = SymEx.parse(pattern)
+      patternEx.map(rewriteRule(_, replace)).getOrElse(throw Error(s"invalid pattern: $pattern"))
+
+    def applyRules(ex: SymEx): Option[lang.Expr] =
+      def step(rules: List[Parser]): Option[lang.Expr] =
+        rules match
+        case r :: rem => r(ex).orElse(step(rem))
+        case Nil => None
+      step(rules())
+
     def parse(ex: SymEx): Option[lang.Expr] =
+      applyRules(ex).orElse(applySymbol(ex))
+
+    def applySymbol(ex: SymEx): Option[lang.Expr] =
       val creators = builtins()
       ex.id().flatMap(id =>
         val create = creators.lookup(id)
@@ -31,6 +54,19 @@ package StringL:
        case Tree.Node(List(Tree.Leaf("plus"), lhs, rhs)) =>
          parse(lhs).flatMap(l => parse(rhs).map(r => lang.plus(l, r)))
        case _ => None)
+
+  extension[T, L[T] <: tfi.Calc_int.Lang[T]] (p: ParseLang[T, L])
+    def calcIntRules() =
+      List(p.rewriteRule("(plus $lhs $rhs)", args => p.lang.plus(args("$lhs"), args("$rhs"))))
+
+  extension[T, L[T] <: tfi.Calc_bool.Lang[T]] (p: ParseLang[T, L])
+    def calcBoolRules() =
+      List(p.rewriteRule("(and $lhs $rhs)", args => p.lang.and(args("$lhs"), args("$rhs"))))
+
+  extension[T, L[T] <: tfi.Calc.Lang[T]] (p: ParseLang[T, L])
+    def calcRules() =
+      p.calcIntRules() ++ p.calcBoolRules() ++
+      List(p.rewriteRule("(greaterThan $lhs $rhs)", args => p.lang.greaterThan(args("$lhs"), args("$rhs"))))
 
   def parseIntValue[T](using lang: tfi.Calc_int.Lang[T])(str: String): Option[lang.Expr] =
       str.toIntOption.map(lang.int)
@@ -64,19 +100,22 @@ package StringL:
     import tfi.Calc_bool.{given}
     import tfi.Calc.{given}
     given ParseLang[Int, Calc_int.Lang](summon[Calc_int.Lang[Int]]) with
-      def addBuiltins(s: SymbolTable): Unit =
-        addBuiltinsCalcInt(using lang)(s, parse)
+      def addBuiltins(s: SymbolTable): Unit = ()
+      override def rules() = this.calcIntRules()
       def parseString(str: String): Option[lang.Expr] = parseIntValue(using lang)(str)
     given ParseLang[Boolean, Calc_bool.Lang](summon[Calc_bool.Lang[Boolean]]) with
-      def addBuiltins(s: SymbolTable): Unit =
-        addBuiltinsCalcBool(using lang)(s, parse)
+      def addBuiltins(s: SymbolTable): Unit = ()
+        // addBuiltinsCalcBool(using lang)(s, parse)
+      override def rules() = this.calcBoolRules()
       def parseString(str: String): Option[lang.Expr] = parseBoolValue(using lang)(str)
     given ParseLang[Calc.Value, Calc.Lang](summon[Calc.Lang[Calc.Value]]) with
-      def addBuiltins(s: SymbolTable): Unit =
-        addBuiltinsCalcInt(using lang)(s, parse)
-        addBuiltinsCalcBool(using lang)(s, parse)
-        addBuiltinsCalc(using lang)(s, parse)
+      def addBuiltins(s: SymbolTable): Unit = ()
       def parseString(str: String): Option[lang.Expr] = parseCalcValue(using lang)(str)
+      override def rules() = List(
+        rewriteRule("(plus $lhs $rhs)", args => lang.plus(args("$lhs"), args("$rhs"))),
+        rewriteRule("(and $lhs $rhs)", args => lang.and(args("$lhs"), args("$rhs"))),
+        rewriteRule("(greaterThan $lhs $rhs)", args => lang.greaterThan(args("$lhs"), args("$rhs")))
+      )
     val intExs = List("10", "plus 10 20", "plus (plus 5 5) 20")
     val boolExs = List("true", "false", "and true false", "and (and true false) true")
     val calcExs = intExs ++ boolExs ++ List("and (greaterThan (plus 1 2) 2) true")
