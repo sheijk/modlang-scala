a68d09723ed09ca79e68e947ed6194f1dd16a4bc
Jan Rehders <nospam@sheijk.net>
2024-03-03 01:43:10 +0000

    Parser for Calc
    Restructered code a bit to make it possible to re-use it

--- src/main/scala/me/modlang/macros/StringL.scala
+++ src/main/scala/me/modlang/macros/StringL.scala
@@ -10,7 +10,11 @@ package StringL:
   trait ParseLang[T, L[T] <: tfi.Empty.Lang[T]](val lang: L[T]):
     type Parser = SymEx => Option[lang.Expr]
     type SymbolTable = Scope[Parser]
-    def builtins(): SymbolTable
+    def builtins(): SymbolTable =
+      val s = SimpleScope[Parser](None)
+      addBuiltins(s)
+      s
+    def addBuiltins(s: SymbolTable): Unit
     def parseString(str: String): Option[lang.Expr]
 
     def parse(ex: SymEx): Option[lang.Expr] =
@@ -21,46 +25,37 @@ package StringL:
         case Some(f) => f(ex)
         case None => parseString(id))
 
-  trait ParseCalcInt[T] extends ParseLang[T, tfi.Calc_int.Lang]:
-    def builtins(): SymbolTable =
-     val s = SimpleScope[Parser](None)
+  def addBuiltinsCalcInt[T](using lang: tfi.Calc_int.Lang[T])(s: Scope[SymEx => Option[lang.Expr]], parse: SymEx => Option[lang.Expr]): Unit =
      s.register("plus", ex =>
        ex match
        case Tree.Node(List(Tree.Leaf("plus"), lhs, rhs)) =>
          parse(lhs).flatMap(l => parse(rhs).map(r => lang.plus(l, r)))
        case _ => None)
-     s
 
-    def parseString(str: String): Option[lang.Expr] =
+  def parseIntValue[T](using lang: tfi.Calc_int.Lang[T])(str: String): Option[lang.Expr] =
       str.toIntOption.map(lang.int)
 
-  trait ParseCalcBool[T] extends ParseLang[T, tfi.Calc_bool.Lang]:
-    def builtins(): SymbolTable =
-     val s = SimpleScope[Parser](None)
-     s.register("plus", ex =>
+  def addBuiltinsCalcBool[T](using lang: tfi.Calc_bool.Lang[T])(s: Scope[SymEx => Option[lang.Expr]], parse: SymEx => Option[lang.Expr]): Unit =
+     s.register("and", ex =>
        ex match
        case Tree.Node(List(Tree.Leaf("and"), lhs, rhs)) =>
          parse(lhs).flatMap(l => parse(rhs).map(r => lang.and(l, r)))
        case _ => None)
-     s
 
-    def parseString(str: String): Option[lang.Expr] =
+  def parseBoolValue[T](using lang: tfi.Calc_bool.Lang[T])(str: String): Option[lang.Expr] =
       if str == "true" then Some(lang.bool(true))
       else if str == "false" then Some(lang.bool(false))
       else None
 
-  trait ParseCalc[T] extends ParseLang[T, tfi.Calc.Lang], ParseCalcBool[T], ParseCalcInt[T]:
-    override def builtins(): SymbolTable =
-     val s = SimpleScope[Parser](None)
-     s.register("plus", ex =>
+  def addBuiltinsCalc[T](using lang: tfi.Calc.Lang[T])(s: Scope[SymEx => Option[lang.Expr]], parse: SymEx => Option[lang.Expr]): Unit =
+     s.register("greaterThan", ex =>
        ex match
        case Tree.Node(List(Tree.Leaf("greaterThan"), lhs, rhs)) =>
          parse(lhs).flatMap(l => parse(rhs).map(r => lang.greaterThan(l, r)))
        case _ => None)
-     s
 
-    override def parseString(str: String): Option[lang.Expr] =
-      super[ParseCalcBool].parseString(str).orElse(super[ParseCalcInt].parseString(str))
+  def parseCalcValue[T](using lang: tfi.Calc.Lang[T])(str: String): Option[lang.Expr] =
+      parseBoolValue(str).orElse(parseIntValue(str))
 
   def demo() =
     println("Parsing tfi language")
@@ -68,17 +63,26 @@ package StringL:
     import tfi.Calc_int.{given}
     import tfi.Calc_bool.{given}
     import tfi.Calc.{given}
-    given ParseCalcInt[Int] with ParseLang[Int, Calc_int.Lang](summon[Calc_int.Lang[Int]]) with {}
-    given ParseCalcBool[Boolean] with ParseLang[Boolean, Calc_bool.Lang](summon[Calc_bool.Lang[Boolean]]) with {}
-// [error] -- Error: /Users/jr/Documents/Development/modular-lang-proto/external/temp/modlang/src/main/scala/me/modlang/macros/StringL.scala:73:10 
-// [error] 73 |    given ParseCalc[Calc.Value] with ParseLang[Calc.Value, Calc.Lang](summon[Calc.Lang[Calc.Value]]) with {}
-// [error]    |          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-// [error]    |object given_ParseCalc_Value_ParseLang_Value_Lang cannot be instantiated since it has conflicting base types me.modlang.macro_compiler.StringL.ParseLang[me.modlang.tfi.Calc.Value, me.modlang.tfi.Calc_bool.Lang] &
-// [error]    |  me.modlang.macro_compiler.StringL.ParseLang[me.modlang.tfi.Calc.Value, me.modlang.tfi.Calc_int.Lang] and me.modlang.macro_compiler.StringL.ParseLang[me.modlang.tfi.Calc.Value, me.modlang.tfi.Calc.Lang]
-    given ParseCalc[Calc.Value] with ParseLang[Calc.Value, Calc.Lang](summon[Calc.Lang[Calc.Value]]) with {}
-    run[Int, tfi.Calc_int.Lang]("10", "plus 10 20", "plus (plus 5 5) 20")
-    run[Boolean, tfi.Calc_bool.Lang]("true", "false", "and true (or true false)")
-    run[Calc.Value, tfi.Calc.Lang]("true", "false", "and true (or true false)")
+    given ParseLang[Int, Calc_int.Lang](summon[Calc_int.Lang[Int]]) with
+      def addBuiltins(s: SymbolTable): Unit =
+        addBuiltinsCalcInt(using lang)(s, parse)
+      def parseString(str: String): Option[lang.Expr] = parseIntValue(using lang)(str)
+    given ParseLang[Boolean, Calc_bool.Lang](summon[Calc_bool.Lang[Boolean]]) with
+      def addBuiltins(s: SymbolTable): Unit =
+        addBuiltinsCalcBool(using lang)(s, parse)
+      def parseString(str: String): Option[lang.Expr] = parseBoolValue(using lang)(str)
+    given ParseLang[Calc.Value, Calc.Lang](summon[Calc.Lang[Calc.Value]]) with
+      def addBuiltins(s: SymbolTable): Unit =
+        addBuiltinsCalcInt(using lang)(s, parse)
+        addBuiltinsCalcBool(using lang)(s, parse)
+        addBuiltinsCalc(using lang)(s, parse)
+      def parseString(str: String): Option[lang.Expr] = parseCalcValue(using lang)(str)
+    val intExs = List("10", "plus 10 20", "plus (plus 5 5) 20")
+    val boolExs = List("true", "false", "and true false", "and (and true false) true")
+    val calcExs = intExs ++ boolExs ++ List("and (greaterThan (plus 1 2) 2) true")
+    run[Int, tfi.Calc_int.Lang](intExs*)
+    run[Boolean, tfi.Calc_bool.Lang](boolExs*)
+    run[Calc.Value, tfi.Calc.Lang](calcExs*)
 
   def run[T, L[T] <: tfi.Empty.Lang[T]](src: String*)(using eval : L[T], parser : ParseLang[T, L]) =
     def p(src: String) =
@@ -87,5 +91,6 @@ package StringL:
       case Some(ex) =>
         println(s"  eval(${src}) = ${parser.lang.eval(ex)}")
       case None =>
-        println(s"failed to parse $src")
+        println(s"  failed to parse $src")
+    println("  " + eval.toString)
     src.foreach(p)
