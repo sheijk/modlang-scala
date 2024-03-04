84ed2fd395130adbc386be80b7263305b277959f
Jan Rehders <nospam@sheijk.net>
2024-03-04 00:44:29 +0000

    Fixed printer

--- src/main/scala/me/modlang/tfi/design.scala
+++ src/main/scala/me/modlang/tfi/design.scala
@@ -19,19 +19,21 @@ package NewDesign:
 
   trait FuncL[E[T]] extends AddL[E], NegL[E], LambdaL[E]
 
-  type StringE[T] = String
+  type StringE[T] = Int => String
 
   case class Show() extends FuncL[StringE]:
     type E[T] = StringE[T]
-    override def int[Env](v: Int): E[Int] = v.toString
-    override def add[Env](lhs: E[Int], rhs: E[Int]): E[Int] = s"($lhs + $rhs)"
-    override def mul[Env](lhs: E[Int], rhs: E[Int]): E[Int] = s"($lhs * $rhs)"
-    override def neg[Env](e: E[Int]): E[Int] = s"-($e)"
+    override def int[Env](v: Int): E[Int] = _ => v.toString
+    override def add[Env](lhs: E[Int], rhs: E[Int]): E[Int] = c => s"(${lhs(c)} + ${rhs(c)})"
+    override def mul[Env](lhs: E[Int], rhs: E[Int]): E[Int] = c => s"(${lhs(c)} * ${rhs(c)})"
+    override def neg[Env](e: E[Int]): E[Int] = c => s"-(${e(c)})"
     override def lambda[I, O](body: E[I] => E[O]): E[I => O] =
-      val bd = body("v")
-      s"(v => $bd)"
+      c =>
+        val bd = body(_ => s"x.$c")
+        s"(x.$c => ${bd(c+1)})"
     override def call[R, T](f: E[T => R], x: E[T]): E[R] =
-      s"($f $x)"
+      c => s"(${f(c)} ${x(c)})"
+  def show[T](e: (l: FuncL[StringE]) => StringE[T]) = e(Show())(0)
 
   type Run[T] = T
 
@@ -43,20 +45,21 @@ package NewDesign:
     override def neg[Env](e: E[Int]): E[Int] = -e
     override def lambda[I, O](body: E[I] => E[O]): E[I => O] = x => body(x)
     override def call[R, T](f: E[T => R], x: E[T]): E[R] = f(x)
+  def eval[T](e: (l: FuncL[Run]) => T) = e(Eval())
 
   def demo() =
     println("NewDesign")
     def a() =
       def test[E[_]](l: FuncL[E]): E[Int] = l.add(l.int(2), l.int(3))
-      println(s"  eval(${test(Show())}) = ${test(Eval())}")
+      println(s"  eval(${show(test)}) = ${eval(test)}")
     def b() =
       def test[E[_]](l: FuncL[E]) = l.call(l.lambda(v => l.add(l.int(2), v)), l.int(5))
-      println(s"  eval(${test(Show())}) = ${test(Eval())}")
+      println(s"  eval(${show(test)}) = ${eval(test)}")
     def c() =
       def test[E[_]](l: FuncL[E]) =
-        val foo = l.lambda((a: E[Int]) => l.lambda((b: E[Int]) => l.mul(a, b)))
+        val foo = l.lambda((a: E[Int]) => l.lambda((b: E[Int]) => l.add(a, l.mul(b, a))))
         l.call(l.call(foo, l.int(10)), l.int(5))
-      println(s"  eval(${test(Show())}) = ${test(Eval())}")
+      println(s"  eval(${show(test)}) = ${eval(test)}")
     a()
     b()
     c()
